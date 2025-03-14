3851497b94ac8db811f1bb8ea8f405cc5d59e5e4
jayslen <jayslen134@gmail.com>
2025-03-13 22:32:40 -0400

    add: integrate JSON middleware and set up posts router

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
-import express from 'express'
+import express, { json } from 'express'
+import { postRouter } from './routes/posts.js'
 
 const app = express()
 const port = 3000
+
+app.use(json())
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello World!</h1>')
 })
 
+app.use('/posts', postRouter)
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`)
 })

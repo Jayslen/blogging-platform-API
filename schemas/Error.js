9bf4826d867b0f74657dead1abf6ea304a658e99
jayslen <jayslen134@gmail.com>
2025-03-13 22:27:00 -0400

    fix: enhance error classes to include custom names in constructors

--- schemas/Error.js
+++ schemas/Error.js
@@ -1,28 +1,26 @@
 class RequestError extends Error {
-  constructor (message, statusCode) {
+  constructor (message, statusCode, name) {
     super(message)
     this.statusCode = statusCode
+    this.name = name
   }
 }
 
 class WriteFileError extends RequestError {
   constructor (message = 'Something went wrong while writing the data') {
-    super(message, 500)
-    this.name = 'WriteFileError'
+    super(message, 500, "'WriteFileError'")
   }
 }
 
 class ResourceNotFoundError extends RequestError {
   constructor (message = 'The resource requested does not exits') {
-    super(message, 404)
-    this.name = 'ResourceNotFound'
+    super(message, 404, 'ResourceNotFound')
   }
 }
 
 class ValidationError extends RequestError {
   constructor (conflicts, message = 'Errors found while parsing the data') {
-    super(message, 400)
-    this.name = 'ValidationError'
+    super(message, 400, 'ValidationError')
     this.conflicts = conflicts
   }
 }
